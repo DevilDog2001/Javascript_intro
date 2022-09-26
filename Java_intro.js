55dfb03e03b661b85db81120b5d18fbebe7593a9
Richardson Antoine <91235784+DevilDog2001@users.noreply.github.com>
2022-09-26 09:50:32 -0400

    Add files via upload

---
+++ Java_intro.js
@@ -0,0 +1,16 @@
+var first = "Richardson "; 
+var last = "Antoine";
+console.log("my name is " + first + last);
+console.log("Hi " + 5 + 5)
+console.log(5 + 5 + " Hi")
+var message = "Hello ";
+console.log(message);
+message += "World ";
+console.log(message);
+console.log(3*(2+1));
+function add(num3, num4) {
+    sum = num3 + num4
+    console.log(sum)
+    return sum
+}
+add(2,3)
